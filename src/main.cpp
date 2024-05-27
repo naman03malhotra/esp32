a1d91010e3d05e358dc42922608fe7996c590bf0
Naman Malhotra <naman03malhotra@gmail.com>
2024-05-27 18:23:44 +0530

    script improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include <WiFi.h>            /* WiFi library for ESP32 */
+#include <WiFi.h> /* WiFi library for ESP32 */
 #include <Wire.h>
 #include <PubSubClient.h>
 #include <Arduino.h>
@@ -7,6 +7,11 @@
 
 #define PUMP_PIN 22
 #define LED_PIN 23
+#define SOIL_MOISTURE_PIN 32
+#define SYSTEM_UP_PIN 26
+#define SOIL_MOISTURE_THRESHOLD 2100
+#define TIME_TO_PUMP 45
+#define TIME_TO_WAIT 30
 
 #define wifi_ssid "Deco 804 Mesh"
 #define wifi_password "yoman33333333"
@@ -21,7 +26,8 @@ void reconnect();
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup_wifi() {
+void setup_wifi()
+{
   WiFi.mode(WIFI_STA);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -30,13 +36,13 @@ void setup_wifi() {
 
   WiFi.begin(wifi_ssid, wifi_password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 
-
-    // Port defaults to 3232
+  // Port defaults to 3232
   // ArduinoOTA.setPort(3232);
 
   // Hostname defaults to esp3232-[MAC]
@@ -50,7 +56,8 @@ void setup_wifi() {
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
   ArduinoOTA
-    .onStart([]() {
+      .onStart([]()
+               {
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH) {
         type = "sketch";
@@ -59,15 +66,13 @@ void setup_wifi() {
       }
 
       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
+      Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
       Serial.printf("Error[%u]: ", error);
       if (error == OTA_AUTH_ERROR) {
         Serial.println("Auth Failed");
@@ -79,8 +84,7 @@ void setup_wifi() {
         Serial.println("Receive Failed");
       } else if (error == OTA_END_ERROR) {
         Serial.println("End Failed");
-      }
-    });
+      } });
 
   ArduinoOTA.begin();
 
@@ -90,14 +94,19 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() {
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
-        Serial.print("Attempting MQTT connection...");
-    
-  if (client.connect("ESP32Client")) {
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("ESP32Client"))
+    {
       Serial.println("connected");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -106,45 +115,68 @@ void reconnect() {
   }
 }
 
-void setup() {
-  Serial.begin(921600);
+void setup()
+{
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-}
-
-void loop() {
-  ArduinoOTA.handle();
   pinMode(PUMP_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(SOIL_MOISTURE_PIN, INPUT);
+  pinMode(SYSTEM_UP_PIN, OUTPUT);
 
-  if (!client.connected()) {
+  digitalWrite(SYSTEM_UP_PIN, HIGH);
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 
-  delay(2000);
-  digitalWrite(PUMP_PIN, HIGH);
-  digitalWrite(LED_PIN, HIGH);
-
-  
-  int is_sensor_on = 1;
-  
-  Serial.print("\nOn: ");
-  Serial.print(is_sensor_on);
-
-  String msg = "{\"st\":\"on\",\"status\":" + String(is_sensor_on) + "}";
-
+  delay(1000);
+  int soil_moisture = analogRead(SOIL_MOISTURE_PIN);
+  int is_sensor_on = 0;
+  String msg = "";
+  msg = "{\"status\":" + String(is_sensor_on) + ", \"soil_moisture\":" + String(soil_moisture) + "}";
   client.publish(topic, msg.c_str(), true);
 
-  delay(2000);
-  digitalWrite(PUMP_PIN, LOW);
-  digitalWrite(LED_PIN, LOW);
+  while (soil_moisture > SOIL_MOISTURE_THRESHOLD)
+  {
+    digitalWrite(PUMP_PIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
+    is_sensor_on = 1; // ON
 
-  is_sensor_on = 0;
-  Serial.print("\nOff: ");
-  Serial.print(is_sensor_on);
+    for (int i = 0; i < TIME_TO_PUMP; i++)
+    { // Loop for 60 seconds, publishing every second
+      soil_moisture = analogRead(SOIL_MOISTURE_PIN);
+      msg = "{\"status\":" + String(is_sensor_on) + ", \"soil_moisture\":" + String(soil_moisture) + "}";
+      client.publish(topic, msg.c_str(), true);
+      delay(1000);
+    }
 
-  msg = "{\"st\":\"off\",\"status\":" + String(is_sensor_on) + "}";
+    digitalWrite(PUMP_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
+    is_sensor_on = 2; // WAIT
 
+    for (int i = 0; i < TIME_TO_WAIT; i++)
+    { // Loop for another 30 seconds, publishing every second
+      soil_moisture = analogRead(SOIL_MOISTURE_PIN);
+      msg = "{\"status\":" + String(is_sensor_on) + ", \"soil_moisture\":" + String(soil_moisture) + "}";
+      client.publish(topic, msg.c_str(), true);
+      delay(1000);
+    }
+
+    soil_moisture = analogRead(SOIL_MOISTURE_PIN);
+    msg = "{\"status\":" + String(is_sensor_on) + ", \"soil_moisture\":" + String(soil_moisture) + "}";
+    client.publish(topic, msg.c_str(), true);
+  }
+
+  is_sensor_on = 0; // OFF
+  soil_moisture = analogRead(SOIL_MOISTURE_PIN);
+  msg = "{\"status\":" + String(is_sensor_on) + ", \"soil_moisture\":" + String(soil_moisture) + "}";
   client.publish(topic, msg.c_str(), true);
+  delay(1000);
 }
