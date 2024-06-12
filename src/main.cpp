17c30987aa4caf444374460806ed378f7a500330
Naman <naman03malhotra@gmail.com>
2024-06-12 22:55:41 +0530

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,11 @@
 #define SOIL_MOISTURE_PIN 32
 #define SYSTEM_UP_PIN 26
 // #define SOIL_MOISTURE_THRESHOLD 500
+#define TIME_TO_WATER "17:00:00"
 #define SOIL_MALFUNCTION_CONSTANT 4095
 #define TIME_TO_PUMP 60
 #define TIME_TO_WAIT 60
-#define SOIL_READING_INTERVAL 20
+#define SOIL_READING_INTERVAL 30
 
 #define wifi_ssid "Deco 804 Mesh"
 #define wifi_password "yoman33333333"
@@ -233,7 +234,7 @@ void loop()
     }
 
     // if (soil_moisture >= SOIL_MOISTURE_THRESHOLD)
-    if (currentTime == "17:00:00") // water plant at 5pm everyday
+    if (currentTime == TIME_TO_WATER) // water plant at 5pm everyday
     {
       digitalWrite(PUMP_PIN, HIGH);
       digitalWrite(LED_PIN, HIGH);
